edba87e93a48656dc5c807f672782100938f6e8a
Edldev12 <edlawitdigital3@gmail.com>
2026-06-16 12:13:35 +0300

    Initial commit - Java Calculator

---
+++ calculater.java
@@ -0,0 +1,68 @@
+import java.util.Scanner;
+
+public class calculater {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+
+            System.out.println("\n>>>>>>>>>>>>>>>>> Calculator >>>>>>>>>>>>>>>>>>");
+            System.out.println("1. Addition");
+            System.out.println("2. Subtraction");
+            System.out.println("3. Multiplication");
+            System.out.println("4. Division");
+            System.out.println("5. Modulus");
+            System.out.println("6. Exit");
+
+            System.out.print("Enter your choice: ");
+            int choice = scanner.nextInt();
+
+            if (choice == 6) {
+                System.out.println("Exiting the program...");
+                break;
+            }
+
+            System.out.print("Enter first number: ");
+            double num1 = scanner.nextDouble();
+
+            System.out.print("Enter second number: ");
+            double num2 = scanner.nextDouble();
+
+            switch (choice) {
+                case 1:
+                    System.out.println("Result: " + (num1 + num2));
+                    break;
+
+                case 2:
+                    System.out.println("Result: " + (num1 - num2));
+                    break;
+
+                case 3:
+                    System.out.println("Result: " + (num1 * num2));
+                    break;
+
+                case 4:
+                    if (num2 != 0)
+                        System.out.println("Result: " + (num1 / num2));
+                    else
+                        System.out.println("Error: Division by zero");
+                    break;
+
+                case 5:
+                    if (num2 != 0)
+                        System.out.println("Result: " + (num1 % num2));
+                    else
+                        System.out.println("Error: Division by zero");
+                    break;
+
+                default:
+                    System.out.println("Invalid choice.");
+            }
+        }
+
+        scanner.close();
+    }
+}
+
+ 
